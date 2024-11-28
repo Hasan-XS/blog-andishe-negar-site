c1d379ecc67240b111775314c88cde318d06b0be
Hasan-XS <ha3an.majidii@gmail.com>
2024-11-28 23:13:47 +0330

    Create models for category, tag for posts

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    featured_image = models.ImageField(upload_to='blog_images/', null=True, blank=True)
+    categories = models.ManyToManyField(Category, related_name='posts')
+    tags = models.ManyToManyField(Tag, related_name='posts')
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    status = models.CharField(max_length=10, choices=[('draft', 'Draft'), ('published', 'Published')], default='draft')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
